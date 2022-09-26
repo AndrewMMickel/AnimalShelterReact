0caab6d45c2ff29e44013df5f565e056e5a483a2
AndrewMMicke <andrew.m.mickel@gmail.com>
2022-09-26 13:30:16 -0700

    clone Repo and add navigation tab

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+import TicketControl from "./TicketControl";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
+  );
+}
+
+export default App;
